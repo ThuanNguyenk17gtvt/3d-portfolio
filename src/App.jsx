064e697dcf74223e90e0e9f508a41caff4c41152
SBA_Thuan <102499997+NguyenDucThuanSBA@users.noreply.github.com>
2023-06-30 14:23:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
